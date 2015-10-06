eb2c6f74887788fde4614e3ca7d9ccb649786f51
dzhu <dzhu@linux-pg7m.dyn.ini.usc.edu>
2015-10-05 18:04:34 -0700

    update

--- djUSC/src/edu/usc/ini/igc/journal/EmbededSC/J_GenerateAvgVol.java
+++ djUSC/src/edu/usc/ini/igc/journal/EmbededSC/J_GenerateAvgVol.java
@@ -8,16 +8,16 @@ import edu.uga.liulab.djVtkBase.djNiftiData;
 
 public class J_GenerateAvgVol {
 
-	public void generateAvgVol(int startSubID, int endSubID, String comID)
+	public void generateAvgVol(String inputDirPre, int startSubID,
+			int endSubID, String comID, String outputFile)
 			throws InvalidImageException, IOException {
-		String avgVolFile = "/ifs/loni/faculty/thompson/four_d/dzhu/Journal_ESL/results/MOTOR/1_68/"
-				+ startSubID + "/map2vol/Com_" + comID + "_std.nii.gz";
-		System.out.println("Loading "+avgVolFile);
+		float subNum = endSubID-startSubID+1;
+		String avgVolFile = inputDirPre + "/"+startSubID+"/map2vol/Com_" + comID + "_std.nii.gz";
+		System.out.println("Loading " + avgVolFile);
 		djNiftiData avgData = new djNiftiData(avgVolFile);
 		for (int s = startSubID + 1; s <= endSubID; s++) {
-			String culVolFile = "/ifs/loni/faculty/thompson/four_d/dzhu/Journal_ESL/results/MOTOR/1_68/"
-					+ s + "/map2vol/Com_" + comID + "_std.nii.gz";
-			System.out.println("Loading "+culVolFile);
+			String culVolFile = inputDirPre + "/"+startSubID+"/map2vol/Com_" + comID + "_std.nii.gz";
+			System.out.println("Loading " + culVolFile);
 			djNiftiData curData = new djNiftiData(culVolFile);
 
 			for (int x = 0; x < avgData.xSize; x++)
@@ -28,28 +28,30 @@ public class J_GenerateAvgVol {
 								x, y, z, 0)
 								+ curData.getValueBasedOnVolumeCoordinate(x, y,
 										z, 0);
-						avgData.rawNiftiData.putPix(tmpSub,
+						avgData.rawNiftiData.putPix(tmpSub/subNum,
 								seedVolCoordsReverse);
 					} // for z
 		} // for all subjects
-		avgData.rawNiftiData
-				.write("/ifs/loni/faculty/thompson/four_d/dzhu/Journal_ESL/results/MOTOR/1_68/stdResult/avg_"
-						+ startSubID + "_" + endSubID + "_com_" + comID);
+		avgData.rawNiftiData.write(outputFile);
 
 	}
 
 	public static void main(String[] args) throws InvalidImageException,
 			IOException {
 
-		if (args.length == 3) {
-			int startSubID = Integer.valueOf(args[0].trim());
-			int endSubID = Integer.valueOf(args[1].trim());
-			String comID = args[2].trim();
+		if (args.length == 5) {
+			String inputDirPre = args[0].trim();
+			int startSubID = Integer.valueOf(args[1].trim());
+			int endSubID = Integer.valueOf(args[2].trim());
+			String comID = args[3].trim();
+			String outputFile = args[4].trim();
 
 			J_GenerateAvgVol mainHandler = new J_GenerateAvgVol();
-			mainHandler.generateAvgVol(startSubID, endSubID, comID);
+			mainHandler.generateAvgVol(inputDirPre, startSubID, endSubID,
+					comID, outputFile);
 		} else
-			System.out.println("Need para: startSubID endSubID comID");
+			System.out
+					.println("Need para: inputDirPre startSubID endSubID comID outputFile");
 
 	}
 
