07057a1dae52d8b90f71eb162ee7f0d4ec662757
dzhu <dzhu@linux-pg7m.dyn.ini.usc.edu>
2015-10-08 09:48:52 -0700

    more

---
+++ djUSC/src/edu/usc/ini/igc/journal/EmbededSC/ESL_ExploreTemplate_AnalyseVR.java
@@ -0,0 +1,260 @@
+package edu.usc.ini.igc.journal.EmbededSC;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.uga.DICCCOL.DicccolUtilIO;
+import edu.uga.DICCCOL.stat.Correlation;
+
+public class ESL_ExploreTemplate_AnalyseVR {
+
+	public int optDicIndex;
+	public int dicSize;
+	public int subStartID = 1;
+	public int subEndID = 68;
+	public int subNum = 0;
+	public double voteRateThreshold = 0.5;
+	public double pValueThreshold = 0.5;
+	public String ESL_OutPutDir = "";
+
+	public boolean foundTemplate = false;
+	public double savedVR = 0.0;
+	public int savedSubID = -1;
+	public int savedDicIndex = -1;
+
+	List<List<String>> allVRList = new ArrayList<List<String>>();
+	public int tSize = 284;
+	public List<double[][]> allDMatrix = new ArrayList<double[][]>();
+	Correlation correlation = null;
+
+	public double[][] trsposeM(double[][] data, int row, int column) {
+		double[][] result = new double[column][row];
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < column; j++)
+				result[j][i] = data[i][j];
+		return result;
+	}
+
+	public void initialization() {
+		System.out
+				.println("######################   Loading File  ##########################");
+		for (int s = subStartID; s <= subEndID; s++) {
+			String curFileVRMap = ESL_OutPutDir
+					+ "/"
+					+ subStartID
+					+ "_"
+					+ subEndID
+					+ "/"
+					+ s
+					+ "/OptDicIndex_"
+					+ optDicIndex
+					+ "/VR_sub_"
+					+ s
+					+ "_OptDicIndex_"
+					+ optDicIndex
+					+ "_start_"
+					+ subStartID
+					+ "_end_" + subEndID + ".txt";
+			System.out.println("Loading : " + curFileVRMap);
+			List<String> curSubVRList = DicccolUtilIO
+					.loadFileToArrayList(curFileVRMap);
+			allVRList.add(curSubVRList);
+		} // for s
+		System.out
+				.println("######################   Loading File End! ##########################");
+		// *************initial correlation handler
+		correlation = new Correlation();
+	}
+
+	public void updateDMatrix() {
+		System.out
+				.println("######################   Updating the DMatrix  ##########################");
+		System.out
+				.println("######################   Loading DMatrix  ##########################");
+		for (int s = subStartID; s <= subEndID; s++) {
+			String configFile = ESL_OutPutDir
+					+ "/"
+					+ subStartID
+					+ "_"
+					+ subEndID
+					+ "/"
+					+ s
+					+ "/config_sub_" + s + ".txt";
+			List<String> configInfo = DicccolUtilIO
+					.loadFileToArrayList(configFile);
+			String[] lastLineArray = configInfo.get(configInfo.size() - 1)
+					.split("\\s+");
+			String curFile = ESL_OutPutDir
+					+ "/"
+					+ subStartID
+					+ "_"
+					+ subEndID
+					+ "/"
+					+ s
+					+ "/OptDicIndex_"
+					+ lastLineArray[0].trim()
+					+ "/sub_"
+					+ s
+					+ "_OptDicIndex_"
+					+ lastLineArray[0].trim()
+					+ "_Round_"
+					+ lastLineArray[1].trim() + "_D.txt";
+			System.out.println("Loading : " + curFile);
+			double[][] curSigM = this.trsposeM(
+					DicccolUtilIO.loadFileAsArray(curFile, tSize, dicSize),
+					tSize, dicSize);
+			allDMatrix.add(curSigM);
+		} // for s
+		System.out
+				.println("######################   Loading DMatrix End! ##########################");
+
+		double[][] candidateMap = new double[subNum][3];
+		candidateMap[savedSubID - subStartID][0] = savedSubID;
+		candidateMap[savedSubID - subStartID][1] = savedDicIndex;
+		candidateMap[savedSubID - subStartID][2] = 1.0;
+		double[] tmplateSig = allDMatrix.get(savedSubID - subStartID)[savedDicIndex];
+		this.updateTemplateInfo(tmplateSig);
+		for (int s = subStartID; s <= subEndID; s++) {
+			double[][] curComparedSubD = allDMatrix.get(s - subStartID);
+			if (s != savedSubID) {
+				double tmpMax = 0.0;
+				for (int tmpD = optDicIndex; tmpD < dicSize; tmpD++) {
+					double tmpCorreValue = correlation.Correlation_Pearsons(
+							tmplateSig, curComparedSubD[tmpD]);
+					if (tmpCorreValue > tmpMax) {
+						candidateMap[s - subStartID][0] = s;
+						candidateMap[s - subStartID][1] = tmpD;
+						candidateMap[s - subStartID][2] = tmpCorreValue;
+						tmpMax = tmpCorreValue;
+					} // if current pValue is larger than threshold
+				} // for tmpD
+			} // if
+
+			// Move candidate signals
+			System.out
+					.println("######################   Moving the candidate signals  ##########################");
+			double[] tmp = curComparedSubD[(int) candidateMap[s - subStartID][1]];
+			curComparedSubD[(int) candidateMap[s - subStartID][1]] = curComparedSubD[optDicIndex];
+			curComparedSubD[optDicIndex] = tmp;
+			System.out
+					.println("######################   Moving the candidate signals end  ##########################");
+			// save updatedDMatrix
+			System.out
+					.println("######################   Saving the updated DMatrix   ##########################");
+			DicccolUtilIO.writeArrayToFile(
+					this.trsposeM(curComparedSubD, dicSize, tSize), tSize,
+					dicSize, " ",
+					ESL_OutPutDir + "/" + subStartID + "_" + subEndID
+							+ "/" + s + "/OptDicIndex_" + optDicIndex + "/sub_"
+							+ s + "_OptDicIndex_" + optDicIndex
+							+ "_Round_1_D.txt");
+			System.out
+					.println("######################   Saving the updated DMatrix end  ##########################");
+			// update config file
+			System.out
+					.println("######################   Updating the config file  ##########################");
+			List<String> configList = DicccolUtilIO
+					.loadFileToArrayList(ESL_OutPutDir
+							+ "/"
+							+ subStartID
+							+ "_"
+							+ subEndID
+							+ "/" + s + "/config_sub_" + s + ".txt");
+			configList.add(optDicIndex + " 1");
+			DicccolUtilIO.writeArrayListToFile(configList,
+					ESL_OutPutDir + "/" + subStartID + "_" + subEndID
+							+ "/" + s + "/config_sub_" + s + ".txt");
+		} // for s
+		System.out
+				.println("######################   Updating the config file end  ##########################");
+		// save candidate information
+		System.out
+				.println("######################   Saving the candidate information   ##########################");
+		DicccolUtilIO.writeArrayToFile(candidateMap, subNum, 3, " ",
+				ESL_OutPutDir + "/" + subStartID + "_" + subEndID
+						+ "/CandidateInfo_OptDicIndex_" + optDicIndex + ".txt");
+		System.out
+				.println("######################   Saving the candidate information end  ##########################");
+	}
+
+	public void updateTemplateInfo(double[] tmplateSig) {
+		List<String> templateList = new ArrayList<String>();
+		for (int t = 0; t < tSize; t++) {
+			String newTmpLine = "" + tmplateSig[t];
+			templateList.add(newTmpLine);
+		}
+		DicccolUtilIO.writeArrayListToFile(templateList,
+				ESL_OutPutDir + "/" + subStartID + "_" + subEndID
+						+ "/TemplateSig_" + optDicIndex + ".txt");
+
+	}
+
+	public void findTheTemplate() {
+		System.out
+				.println("######################   Analysing the VR files  ##########################");
+		double maxPvalue = 0.5;
+		for (int s = subStartID; s <= subEndID; s++) {
+			List<String> curSubVRList = allVRList.get(s - subStartID);
+			for (int i = 0; i < curSubVRList.size(); i++) {
+				String curLine = curSubVRList.get(i);
+				String[] curLineArray = curLine.split("\\s+");
+				double curPValue = Double.valueOf(curLineArray[0].trim());
+				double curVR = Double.valueOf(curLineArray[1].trim());
+				int curDicIndex = Integer.valueOf(curLineArray[2].trim());
+				if (curPValue >= pValueThreshold && curVR >= voteRateThreshold)
+					if (curPValue > maxPvalue) {
+						foundTemplate = true;
+						maxPvalue = curPValue;
+						savedVR = curVR;
+						savedSubID = s;
+						savedDicIndex = curDicIndex;
+					}
+			} // for i
+		} // for s
+		System.out
+				.println("######################   Analysing the VR files end  ##########################");
+		System.out.println("Center subject: " + savedSubID + "   DicIndex: "
+				+ savedDicIndex + "   VR: " + savedVR);
+		if (foundTemplate) {
+			this.updateDMatrix();
+		} // if
+	}
+
+	public void printParaInfo() {
+		System.out.println("***************  Paramater Info  ***************");
+		System.out.println("ESL_OutPutDir: " + ESL_OutPutDir);
+		System.out.println("optDicIndex: " + optDicIndex);
+		System.out.println("dicSize: " + dicSize);
+		System.out.println("subStartID: " + subStartID);
+		System.out.println("subEndID: " + subEndID);
+		System.out.println("subNum: " + subNum);
+		System.out.println("voteRateThreshold: " + voteRateThreshold);
+		System.out.println("pValueThreshold: " + pValueThreshold);
+		System.out.println("************************************************");
+	}
+
+	public static void main(String[] args) {
+		if (args.length == 7) {
+			ESL_ExploreTemplate_AnalyseVR mainHandler = new ESL_ExploreTemplate_AnalyseVR();
+
+			mainHandler.optDicIndex = Integer.valueOf(args[0].trim()); // 0-399
+			mainHandler.dicSize = Integer.valueOf(args[1].trim()); // 400
+			mainHandler.subStartID = Integer.valueOf(args[2].trim()); // 1-58?
+			mainHandler.subEndID = Integer.valueOf(args[3].trim()); // 10-68
+			mainHandler.subNum = mainHandler.subEndID - mainHandler.subStartID
+					+ 1;
+			mainHandler.voteRateThreshold = Double.valueOf(args[4].trim());
+			mainHandler.pValueThreshold = Double.valueOf(args[5].trim());
+			mainHandler.ESL_OutPutDir = args[6].trim();
+
+			mainHandler.printParaInfo();
+			mainHandler.initialization();
+			mainHandler.findTheTemplate();
+		} else
+			System.out
+					.println("Input: optDicIndex (0-399), dicSize(400), subStartID(1-58), subEndID(10-68), voteRateThreshold(0.5), pValueThreshold(0.5) and ESL_OutPutDir");
+
+	}
+
+}
---
+++ djUSC/src/edu/usc/ini/igc/journal/EmbededSC/ESL_ExploreTemplate_CalculateVR.java
@@ -0,0 +1,217 @@
+package edu.usc.ini.igc.journal.EmbededSC;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.uga.DICCCOL.DicccolUtilIO;
+import edu.uga.DICCCOL.stat.Correlation;
+
+public class ESL_ExploreTemplate_CalculateVR {
+
+	public int optDicIndex;
+	public int dicSize;
+	public int currentSubID;
+	public int subStartID = 1;
+	public int subEndID = 68;
+
+	public double pValueThresholdUB = 0.95;
+	public double pValueThresholdLB = 0.5;
+	public double pValueThresholdStep = 0.05;
+	public double pValueCheckThreshold = 0.5;
+	public String ESL_OutPutDir = "";
+
+	public int subNum = 0;
+	public double[][] currentCorrMap;
+	public double[][] previousTemplate;
+	public List<double[][]> allDMatrix = new ArrayList<double[][]>();
+	Correlation correlation = null;
+
+	// ********** pre-set parameters
+	public int tSize = 284;
+
+	public double[][] trsposeM(double[][] data, int row, int column) {
+		double[][] result = new double[column][row];
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < column; j++)
+				result[j][i] = data[i][j];
+		return result;
+	}
+
+	public void initialization() {
+		currentCorrMap = new double[dicSize][dicSize * subNum];
+		System.out
+				.println("######################   Loading File  ##########################");
+		for (int s = subStartID; s <= subEndID; s++) {
+			String configFile = ESL_OutPutDir+"/"+subStartID+"_"+subEndID+"/"+s+"/config_sub_"+s+".txt";
+			List<String> configInfo = DicccolUtilIO.loadFileToArrayList(configFile);
+			String[] lastLineArray = configInfo.get(configInfo.size()-1).split("\\s+");
+			String curFile = ESL_OutPutDir+"/"+subStartID+"_"+subEndID+"/"+s+"/OptDicIndex_"+lastLineArray[0].trim()+"/sub_"+s+"_OptDicIndex_"+lastLineArray[0].trim()+"_Round_"+lastLineArray[1].trim()+"_D.txt";
+			System.out.println("Loading : " + curFile);
+			double[][] curSigM = this.trsposeM(
+					DicccolUtilIO.loadFileAsArray(curFile, tSize, dicSize),
+					tSize, dicSize);
+			allDMatrix.add(curSigM);
+		} // for s
+		System.out
+				.println("######################   Loading File End! ##########################");
+		if (optDicIndex > 0) // if not the first time to find the dictionary
+								// template
+		{
+			System.out
+					.println("######################   Loading previous dictionary templates  ##########################");
+			previousTemplate =  new double[optDicIndex][tSize];
+			for(int i=0;i<optDicIndex;i++)
+			{
+				double[][] tmpSigTemplate = this.trsposeM(DicccolUtilIO
+						.loadFileAsArray(
+								ESL_OutPutDir+"/"+subStartID+"_"+subEndID+"/TemplateSig_"
+										+ i + ".txt", tSize, 1), tSize, 1);
+				previousTemplate[i] = tmpSigTemplate[0];
+			} //for
+			System.out
+					.println("######################   Loading previous dictionary templates end  ##########################");
+		}
+		// *************initial correlation handler
+		correlation = new Correlation();
+	}
+
+	public boolean checkWithPreviousTemplates(double[] sigNeedToBeChecked) {
+		boolean isSimilar = false;
+		for (int i = 0; i < optDicIndex; i++) {
+			double tmpCorreValue = correlation.Correlation_Pearsons(
+					sigNeedToBeChecked, previousTemplate[i]);
+			if (tmpCorreValue >= pValueCheckThreshold) {
+				isSimilar = true;
+				System.out.println("!!!!!!!!!! Similar with the " + i
+						+ " th template !!!!!!!");
+				break;
+			}
+		}
+
+		return isSimilar;
+	}
+
+	public void calCurrentCorreMap() {
+		System.out
+				.println("######################   Calculating the CorreMap  ##########################");
+		for (int d = optDicIndex; d < this.dicSize; d++) {
+			double[] currentD = allDMatrix.get(currentSubID - subStartID)[d];
+			if (optDicIndex > 0)
+				if (this.checkWithPreviousTemplates(currentD))
+					continue;
+			System.out
+					.println("Calculating the " + d + "th dictionary item...");
+			for (int s = subStartID; s <= subEndID; s++) {
+				if (s != currentSubID) {
+					for (int tmpD = optDicIndex; tmpD < this.dicSize; tmpD++) {
+						double[] compareD = allDMatrix.get((s - subStartID))[tmpD];
+						double tmpCorreValue = correlation
+								.Correlation_Pearsons(currentD, compareD);
+						currentCorrMap[d][(s - subStartID) * dicSize + tmpD] = tmpCorreValue;
+					} // for tmpD
+				} // if
+			} // for s
+		} // for d
+		System.out
+				.println("######################   Calculating the CorreMap End!  ##########################");
+	}
+
+	public void findTheTemplate() {
+		System.out
+				.println("######################   Finding the template  ##########################");
+		List<String> VROutPut = new ArrayList<String>();
+		for (double currentThreshold = pValueThresholdUB; currentThreshold >= pValueThresholdLB; currentThreshold = currentThreshold
+				- pValueThresholdStep) {
+			System.out.println(" Calculating currentThreshold: "
+					+ currentThreshold);
+			double maxVR = 0.0;
+			int dicIndexWithMaxVR = -1;
+			for (int d = optDicIndex; d < dicSize; d++) {
+				double tmpVR = 0.0;
+				for (int compareSubID = subStartID; compareSubID <= subEndID; compareSubID++) {
+					if (compareSubID != currentSubID)
+						for (int tmpD = optDicIndex; tmpD < dicSize; tmpD++) {
+							if (currentCorrMap[d][(compareSubID - subStartID)
+									* dicSize + tmpD] >= currentThreshold) {
+								tmpVR++;
+								break;
+							} // if current pValue is larger than threshold
+						} // for tmpD
+				} // for all the subjects to be compared
+				tmpVR = tmpVR / (subNum - 1);
+				if (tmpVR > maxVR) {
+					maxVR = tmpVR;
+					dicIndexWithMaxVR = d;
+				} // if tmpVR>maxVR
+			} // for d
+			VROutPut.add("" + currentThreshold + " " + maxVR + " "
+					+ dicIndexWithMaxVR);
+		} // for each threshold
+		System.out
+				.println("######################   Finding the template end  ##########################");
+
+		System.out
+				.println("######################   Writing the VR info  ##########################");
+		DicccolUtilIO.writeArrayListToFile(VROutPut,
+				ESL_OutPutDir+"/"+subStartID+"_"+subEndID+"/"+currentSubID+"/OptDicIndex_"+optDicIndex+"/VR_sub_" + currentSubID
+						+ "_OptDicIndex_" + optDicIndex + "_start_"
+						+ subStartID + "_end_" + subEndID + ".txt");
+		System.out
+				.println("######################   Writing the VR info end  ##########################");
+	}
+
+	public void printParaInfo() {
+		System.out.println("***************  Paramater Info  ***************");
+		System.out.println("ESL_OutPutDir: " + ESL_OutPutDir);
+		System.out.println("optDicIndex: " + optDicIndex);
+		System.out.println("dicSize: " + dicSize);
+		System.out.println("currentSubID: " + currentSubID);
+		System.out.println("subStartID: " + subStartID);
+		System.out.println("subEndID: " + subEndID);
+		System.out.println("subNum: " + subNum);
+		System.out.println("pValueThresholdUB: " + pValueThresholdUB);
+		System.out.println("pValueThresholdLB: " + pValueThresholdLB);
+		System.out.println("pValueThresholdStep: " + pValueThresholdStep);
+		System.out.println("pValueCheckThreshold: " + pValueCheckThreshold);
+		System.out.println("************************************************");
+	}
+
+	public static void main(String[] args) {
+
+		if (args.length == 10) {
+			ESL_ExploreTemplate_CalculateVR mainHandler = new ESL_ExploreTemplate_CalculateVR();
+
+			mainHandler.optDicIndex = Integer.valueOf(args[0].trim()); // 0-399
+			mainHandler.dicSize = Integer.valueOf(args[1].trim()); // 400
+			mainHandler.currentSubID = Integer.valueOf(args[2].trim()); // [subStartID,subEndID]
+			mainHandler.subStartID = Integer.valueOf(args[3].trim()); // 1-58?
+			mainHandler.subEndID = Integer.valueOf(args[4].trim()); // 10-68
+			mainHandler.subNum = mainHandler.subEndID - mainHandler.subStartID
+					+ 1;
+			mainHandler.pValueThresholdUB = Double.valueOf(args[5].trim());
+			mainHandler.pValueThresholdLB = Double.valueOf(args[6].trim());
+			mainHandler.pValueThresholdStep = Double.valueOf(args[7].trim());
+			mainHandler.pValueCheckThreshold = Double.valueOf(args[8].trim());
+			mainHandler.ESL_OutPutDir = args[9].trim();
+			if (mainHandler.currentSubID < mainHandler.subStartID
+					|| mainHandler.currentSubID > mainHandler.subEndID) {
+				System.out.println("currentSubID should be within the range!");
+				System.exit(0);
+			}
+			if (mainHandler.pValueThresholdUB < mainHandler.pValueThresholdLB) {
+				System.out
+						.println("pValueThresholdUB should be larger than pValueThresholdLB!");
+				System.exit(0);
+			}
+
+			mainHandler.printParaInfo();
+			mainHandler.initialization();
+			mainHandler.calCurrentCorreMap();
+			mainHandler.findTheTemplate();
+		} else
+			System.out
+					.println("Input: optDicIndex (0-399), dicSize(400), currentSubID[subStartID,subEndID], subStartID(1-58), subEndID(10-68), pValueThresholdUB(0.75), pValueThresholdLB(0.5), pValueThresholdStep(0.05), pValueCheckThreshold(0.5) and ESL_OutPutDir");
+
+	}
+
+}
